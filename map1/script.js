1e289121591182cb526916cfe2482bbc07a7d612
SebastianHafner <hafnersailing@gmail.com>
2019-11-17 13:39:43 +0100

    map1 updated

--- map1/script.js
+++ map1/script.js
@@ -12,9 +12,163 @@ var map1DataRequest = $.ajax({
 // making sure data request is complete before rendering map
 $.when(map1DataRequest).done(function() {
 
-    var data = map1DataRequest.responseJSON.data;
-    var country = 'Spanien';
+    var data = map1DataRequest.responseJSON.data,
+        oldSelected = ['Spanien'],
+        newSelected = oldSelected;
+
+
+    data = restructureData(data);
     console.log(data);
+
+
+    $("#multiSelect").select2();
+
+    $('#multiSelect').on('change', function (e) {
+        var options = e.currentTarget.selectedOptions,
+            i;
+
+        // update new selected countries
+        newSelected = [];
+        for (i = 0; i < options.length; i++) {
+            newSelected.push(options[i].label);
+        };
+        console.log(oldSelected, newSelected);
+        updateChart()
+
+        oldSelected = newSelected;
+    });
+
+    // change country selection
+    document
+      .querySelector('#basemaps')
+      .addEventListener('change', function (e) {
+        country = e.target.value;
+        updateChart();
+      });
+
+      // set the dimensions and margins of the graph
+    var margin = {top: 20, right: 20, bottom: 30, left: 50},
+        width = 960 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;
+
+
+
+    // append the svg obgect to the body of the page
+    // appends a 'group' element to 'svg'
+    // moves the 'group' element to the top left margin
+    var svg = d3.select("#chart")
+        .append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+
+    // set the ranges
+    var xScale = d3.scaleTime()
+        .domain(d3.extent(data[oldSelected[0]], function(d) { return d.date; }))
+        .range([0, width]);
+
+
+    var yScale = d3.scaleLinear()
+        .domain([0, d3.max(data[oldSelected[0]], function(d) { return d.value; })])
+        .range([height, 0]);
+
+
+    // Define the axes
+    var xAxis = d3.axisBottom().scale(xScale).ticks(5);
+    var yAxis = d3.axisLeft().scale(yScale).ticks(5);
+
+
+    // define the line
+    var valueline = d3.line()
+        .x(function(d) { return xScale(d.date); })
+        .y(function(d) { return yScale(d.value); });
+
+
+    // Add the valueline path.
+    svg.append("path")
+        .attr("class", "line")
+        .attr("d", valueline(data[oldSelected[0]]));
+
+    // Add the X Axis
+    svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+
+    // Add the Y Axis
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis);
+
+
+    function updateChart() {
+
+        console.log('selected countries', newSelected);
+        console.log('changing to ' + newSelected[0]);
+
+        var cdata = data[newSelected[0]],
+            selectedData = [],
+            i,
+            yMax = 0;
+
+
+        for (i = 0; i < newSelected.length; i++) {
+            selectedData.push(data[newSelected[i]]);
+        };
+
+        console.log(selectedData);
+
+
+        // Select the section we want to apply our changes to
+        var svg = d3.select("#chart").transition();
+
+        // defining transition
+        var t = d3.transition()
+            .duration(750)
+
+
+        // changing y axis
+        for (i = 0; i < selectedData.length; i++) {
+            var countryMax = d3.max(selectedData[i], function(d) { return d.value; });
+            yMax = countryMax > yMax ? countryMax : yMax;
+        };
+        console.log('maxvalue', yMax);
+
+        yScale = d3.scaleLinear()
+            .domain([0, yMax])
+            .range([height, 0]);
+
+        svg.select(".y") // change the y axis
+            .transition(t)
+            .call(yAxis.scale(yScale));
+
+
+        // updating still present classes
+
+
+        // Make the changes
+        svg.select(".line")   // change the line
+            .transition(t)
+            .attr("d", valueline(cdata));
+
+
+        svg = d3.select("#chart");
+        for (i = 0; i < selectedData.length; i++) {
+            // Add the valueline path.
+            console.log(i);
+            svg.append("path")
+                .attr("class", "line" + i)
+                .attr("d", valueline(selectedData[i]));
+        };
+
+
+
+    };
+
+    //    draw(data, country);
+
 
     function restructureData(data) {
 
@@ -40,119 +194,5 @@ $.when(map1DataRequest).done(function() {
         };
         return dataRestructured;
     };
-
-
-    data = restructureData(data);
-    console.log(data);
-
-
-    // change country selection
-    document
-      .querySelector('#basemaps')
-      .addEventListener('change', function (e) {
-        country = e.target.value;
-        update(data, country);
-      });
-
-      // set the dimensions and margins of the graph
-    var margin = {top: 20, right: 20, bottom: 30, left: 50},
-        width = 960 - margin.left - margin.right,
-        height = 500 - margin.top - margin.bottom;
-
-
-
-    // append the svg obgect to the body of the page
-    // appends a 'group' element to 'svg'
-    // moves the 'group' element to the top left margin
-    var svg = d3.select("#chart")
-        .append("svg")
-            .attr("width", width + margin.left + margin.right)
-            .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-
-    var cdata = data[country];
-
-
-    // set the ranges
-    var xScale = d3.scaleTime()
-        .domain(d3.extent(cdata, function(d) { return d.date; }))
-        .range([0, width]);
-
-
-    var yScale = d3.scaleLinear()
-        .domain([0, d3.max(cdata, function(d) { return d.value; })])
-        .range([height, 0]);
-
-
-
-    // y.domain([0, d3.max(cdata, function(d) { return d.value; })]);
-
-
-    // Define the axes
-    var xAxis = d3.axisBottom().scale(xScale).ticks(5);
-    var yAxis = d3.axisLeft().scale(yScale).ticks(5);
-
-
-    // define the line
-    var valueline = d3.svg.line()
-        .x(function(d) { return xScale(d.date); })
-        .y(function(d) { return yScale(d.value); });
-
-
-    // Add the valueline path.
-    svg.append("path")
-        .attr("class", "line")
-        .attr("d", valueline(cdata));
-
-    // Add the X Axis
-    svg.append("g")
-        .attr("transform", "translate(0," + height + ")")
-        .call(xAxis);
-
-    // Add the Y Axis
-    svg.append("g")
-        .attr("class", "y axis")
-        .call(yAxis);
-
-
-    function update(data, country) {
-
-        console.log('changing to ' + country);
-        var cdata = data[country];
-
-
-        // Select the section we want to apply our changes to
-        var svg = d3.select("#chart").transition();
-
-        yScale = d3.scaleLinear()
-            .domain([0, d3.max(cdata, function(d) { return d.value; })])
-            .range([height, 0]);
-
-        yAxis.scale(yScale);
-
-        var t = d3.transition()
-            .duration(750)
-
-        // Make the changes
-        svg.select(".line")   // change the line
-            .duration(750)
-            .attr("d", valueline(cdata));
-
-        svg.select(".x") // change the x axis
-            .transition(t)
-            .call(xAxis);
-
-        svg.select(".y") // change the y axis
-            .transition(t)
-            .call(yAxis);
-
-    };
-
-    //    draw(data, country);
-
-
-
 
 });
