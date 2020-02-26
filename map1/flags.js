72e5f7b9557f9d66fbdf73de2b9901372bab4f01
SebastianHafner <hafnersailing@gmail.com>
2020-02-26 22:52:17 +0100

    removed outdated maps

--- map1/flags.js
+++
@@ -1,6 +0,0 @@
-var urlFlags = {
-    'Spanien': 'https://image.flaticon.com/icons/svg/555/555635.svg',
-    'Argentinien': 'https://image.flaticon.com/icons/svg/555/555615.svg',
-    'Kuba': 'https://image.flaticon.com/icons/svg/555/555550.svg',
-    'Peru': 'https://image.flaticon.com/icons/svg/555/555605.svg',
-};
